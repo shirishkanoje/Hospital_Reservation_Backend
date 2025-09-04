def0799ace872a4a32b80b6878da8f7188c3a074
Shirish Kanoje <152640237+shirishkanoje@users.noreply.github.com>
2025-09-04 19:25:55 +0530

    Delete HospitalReservation directory

--- HospitalReservation/src/main/java/com/shirishkanoje/HospitalReservation/repository/PatientRepository.java
+++
@@ -1,8 +0,0 @@
-package com.shirishkanoje.HospitalReservation.repository;
-
-import com.shirishkanoje.HospitalReservation.model.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientRepository extends JpaRepository<Patient, Long> {
-    // Additional custom queries can be added here if needed
-}
